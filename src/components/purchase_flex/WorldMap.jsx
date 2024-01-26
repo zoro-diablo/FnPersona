b26459a0267a8aecc3e336b6893105380038628c
Vyshnav.K <vyshnavk117@gmail.com>
2024-01-26 09:57:07 +0530

    Update WorldMap component configuration

--- src/components/purchase_flex/WorldMap.jsx
+++ src/components/purchase_flex/WorldMap.jsx
@@ -10,7 +10,7 @@ const WorldMap = () => {
           bbox: [67.177, 36.494, 98.403, 6.965], // get bbox from zingchart.maps.getItemInfo('world-countries','ind');
           ignore: ['IND'], // ignore India because we are rendering a more specific India map below
           name: 'world.countries',
-          panning: false, // turn of zooming. Doesn't work with bounding box
+          panning: true, // turn of zooming. Doesn't work with bounding box
           style: {
             tooltip: {
               borderColor: '#000',
@@ -18,22 +18,23 @@ const WorldMap = () => {
               fontSize: '18px',
             },
             controls: {
-              visible: false, // turn of zooming. Doesn't work with bounding box
+              visible: true, // turn of zooming. Doesn't work with bounding box
             },
             hoverState: {
               alpha: 0.28,
             },
           },
-          zooming: false, // turn of zooming. Doesn't work with bounding box
+          zooming: true, // turn of zooming. Doesn't work with bounding box
         },
       },
+      
       {
         type: 'zingchart.maps',
         options: {
           name: 'ind',
-          panning: false, // turn of zooming. Doesn't work with bounding box
-          zooming: false,
-          scrolling: false,
+          panning: true, // turn of zooming. Doesn't work with bounding box
+          zooming: true,
+          scrolling: true,
           style: {
             backgroundColor: 'black',
             borderColor: 'white',
@@ -44,7 +45,7 @@ const WorldMap = () => {
             },
             borderWidth: '1px',
             controls: {
-              visible: false, // turn of zooming. Doesn't work with bounding box
+              visible: true, // turn of zooming. Doesn't work with bounding box
             },
             hoverState: {
               alpha: 0.5,
@@ -106,14 +107,36 @@ const WorldMap = () => {
         },
       },
     ],
+    plot: {
+      tooltip: {
+        visible: false
+      },
+      hoverState: {
+        visible: true,
+        backgroundColor: '#DFF0D8'
+      },
+      click: function(p) {
+        // Get the bounding box of the clicked state
+        let bbox = zingchart.maps.getItemInfo('world.countries', p.shapeid, 'bbox');
+        
+        // Zoom into the clicked state
+        zingchart.exec('myChart', 'zoomTo', {
+          x: [bbox[0], bbox[2]],
+          y: [bbox[1], bbox[3]]
+        });
+      }
+    },
+    // rest of your configuration
+
   };
   useEffect(() => {
-    zingchart.loadModules('maps,maps-ind,maps-world-countries');
+    zingchart.loadModules('maps,maps-ind');
     zingchart.render({
       id: 'myChart',
       data: chartConfig,
       height: '100%',
       width: '100%',
+      
     });
   }, []);
 
